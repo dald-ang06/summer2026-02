4db8e54466a9ca1ef34e9c72aed35b19bbfc6918
Park Junhee <168513756+ipapee@users.noreply.github.com>
2026-06-23 11:25:59 +0900

    Add comments for alarm class and compareTimeOfDay

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@
 
 // 4. main.cpp: 테스트
 // 1의 본인이름학번의 네임스페이스 안에 비멤버함수 compareTimeOfDay 정의: 매개변수는 const timeOfDay 참조형 2개, 매개변수 멤버들이 모두 같은지를 비교
+// -main
 // alarm 클래스형 객체1 선언, print함수 호출
 // alarm 클래스형 객체2 초기값을 넣어서 선언, print함수 호출
 // 비멤버함수 compareTimeOfDay를 호출하여 그 리턴값이 true면 same, false면 different를 콘솔창으로 출력
