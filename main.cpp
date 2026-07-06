90dffd3e626d825fd17778acd59a9869add9f8df
dald-ang06 <ldy2566038@ewha.ac.kr>
2026-07-06 02:48:50 +0000

    message

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 
-// 4. main.cpp: 테스트
-// 1의 본인이름학번의 네임스페이스 안에 비멤버함수 compareTimeOfDay 정의: 매개변수는 const timeOfDay 참조형 2개, 매개변수 멤버들이 모두 같은지를 비교
-// -main
-// alarm 클래스형 객체1 선언, print함수 호출
-// alarm 클래스형 객체2 초기값을 넣어서 선언, print함수 호출
-// 비멤버함수 compareTimeOfDay를 호출하여 그 리턴값이 true면 same, false면 different를 콘솔창으로 출력
+// // 4. main.cpp: 테스트
+// // 1의 본인이름학번의 네임스페이스 안에 비멤버함수 compareTimeOfDay 정의: 매개변수는 const timeOfDay 참조형 2개, 매개변수 멤버들이 모두 같은지를 비교
+// // -main
+// // alarm 클래스형 객체1 선언, print함수 호출
+// // alarm 클래스형 객체2 초기값을 넣어서 선언, print함수 호출
+// // 비멤버함수 compareTimeOfDay를 호출하여 그 리턴값이 true면 same, false면 different를 콘솔창으로 출력
 
-#include "alarm.h"
-namespace leedayeon2566038{
-    bool compareTimeOfDay(const timeOfDay& t1,const timeOfDay& t2){
-        return t1.getHour()==t2.getHour()&&t1.getMinute()==t2.getMinute();
-    }
-}
+// #include "alarm.h"
+// namespace leedayeon2566038{
+//     bool compareTimeOfDay(const timeOfDay& t1,const timeOfDay& t2){
+//         return t1.getHour()==t2.getHour()&&t1.getMinute()==t2.getMinute();
+//     }
+// }
 
-int main()
-{
+// int main()
+// {
+//     using namespace leedayeon2566038;
+//     alarm a1;
+//     a1.print();
+//     alarm a2{{11,44},1};
+//     a2.print();
+//     if(compareTimeOfDay(a1.getWakeTime(),a2.getWakeTime()))std::cout<<"same\n";
+//     else std::cout<<"different\n";
+// }
+
+
+#include "timeOfDay.h"
+
+int main(){
     using namespace leedayeon2566038;
-    alarm a1;
-    a1.print();
-    alarm a2{{11,44},1};
-    a2.print();
-    if(compareTimeOfDay(a1.getWakeTime(),a2.getWakeTime()))std::cout<<"same\n";
+    timeOfDay t1,t2;
+    std::cin >>t1>>t2;
+    std::cout<<t1<<" "<<t2<<'\n';
+    std::cout<<++t2<<'\n';
+    std::cout<<t2++<<'\n';
+    std::cout<<t2<<'\n';
+    if(t1==t2) std::cout<<"same\n";
     else std::cout<<"different\n";
+
+    std::cout<<t1+t2<<std::endl;
+    return 0;
+
+    
 }
--- timeOfDay.h
+++ timeOfDay.h
@@ -20,11 +20,69 @@ namespace leedayeon2566038{
         }
 
         public:
+    
         timeOfDay(int h=0,int m=0)
         :hour{h},minute{m}{
             testHour();
             testMinute();
         }
+        timeOfDay operator++(){
+            ++minute; //0~59 만약 59이면??
+            if(minute==60) {
+                ++hour; minute=0;
+            }
+            if(hour==24){
+                    hour=0;
+                }
+            return timeOfDay{hour, minute};
+        }
+
+        timeOfDay operator++(int){
+            timeOfDay temp{hour,minute};
+            ++minute; //0~59 만약 59이면??
+            if(minute==60) {
+                ++hour; minute=0;
+            }
+            if(hour==24){
+                    hour=0;
+                }
+            return temp;
+        }
+
+
+        friend std::istream& operator>>(std::istream& is, timeOfDay& t)
+        {
+            std::cout<<"Enter hour(0~23): "; is>>t.hour; t.testHour();
+            std::cout<<"Enter minute(0~59): " ; is>>t.minute; t.testMinute();
+            return is;
+        }
+
+        friend std::ostream& operator<<(std::ostream&os, const timeOfDay& t){
+            if(t.hour<10){
+                os<<"0";
+            }
+            os<<t.hour<<":";
+            if(t.minute<10){
+                os<<"0";
+            }
+            os<<t.minute;
+            return os;
+        }
+
+        friend bool operator ==(const timeOfDay&t1, const timeOfDay& t2){
+            return t1.hour==t2.hour&& t1.minute==t2.minute;
+        }
+
+        friend timeOfDay operator+(const timeOfDay& t1, const timeOfDay& t2){
+            int minute1{t1.minute+t1.hour*60};
+            int minute2{t2.minute+t2.hour*60};
+            int totalMinute{minute1+minute2};
+            int newMinute{totalMinute%60};
+            int newHour{totalMinute/60};
+            if(newHour>24) newHour%=24;
+            return {newHour,newMinute};
+        }
+
         void input(){
             std::cout<<"Enter hour(0~23) ";
             std::cin>>hour; testHour();
